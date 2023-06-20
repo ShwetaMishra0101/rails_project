b543dfb28c2b6108c3e16a0cb4d8135bce91e5e8
Shweta Mishra <shweta.aith@gmail.com>
2023-06-20 14:00:52 +0530

    add navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+require("semantic-ui")
+
+//= require jquery
+//= require semantic-ui
+$(document).on('turbolinks:load', function(){
+    $('.ui .dropdown').dropdown();
+})
+
+// $(document).ready(function() {
+//     $('.ui.dropdown').dropdown();
+//   });

b6b24a4b099423073d642838d2c9dd82b566b1c9
Shweta Mishra <shweta.aith@gmail.com>
2023-06-20 15:21:26 +0530

    Add user models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "shweta", password: "password")
+User.create(username: "araya", password: "password")
+User.create(username: "aradhaya", password: "password")
+User.create(username: "dolly", password: "password")
+User.create(username: "shw", password: "password")
